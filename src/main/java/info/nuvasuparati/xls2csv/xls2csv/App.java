18a6e371c61dcdf7115a4e5e6afb683292730da3
Andrei Cristian Petcu <andrei@ceata.org>
2013-11-04 21:25:39 +0200

    initial tika project

---
+++ src/main/java/info/nuvasuparati/xls2csv/xls2csv/App.java
@@ -0,0 +1,13 @@
+package info.nuvasuparati.xls2csv.xls2csv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
